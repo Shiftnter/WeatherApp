19f61a3490a42514689c8d95589291d9ab2e4789
Kunal Piplani <72797654+Shiftnter@users.noreply.github.com>
2024-06-07 08:51:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ let weather = {
         "Wind speed: " + speed + " km/h";
       document.querySelector(".weather").classList.remove("loading");
       document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1600x900/?" + monument + "')";
+        "url('https://source.unsplash.com/1600x900/?" + sky + "')";
     },
     search: function () {
       this.fetchWeather(document.querySelector(".search-bar").value);
